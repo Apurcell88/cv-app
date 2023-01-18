84f6659d195e4a4498dec5b2979beb80411bc09d
Adam Purcell <apurcell88@gmail.com>
2023-01-18 15:27:54 -0600

    reformatted the JSX for conditional rendering. Added display state and a function for it.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import GeneralInfo from './components/General';
 
 function App() {
+
   return (
     <div className="App">
       <Header />
--- src/components/General.js
+++ src/components/General.js
@@ -1,60 +1,73 @@
 import React from 'react';
 
-const GeneralInfo = () => {
+const GeneralInfo = (props) => {
+  // move state and handleChange to App.js and pass in as props?
   const [info, setInfo] = React.useState({
     name: '',
     email: '',
     phoneNumber: ''
   });
 
+  const [displayInfo, setDisplayInfo] = React.useState(false);
+
   const handleChange = (e) => {
     // update state based on values from inputs
+    const { id, value } = e.target;
     setInfo(prevInfo => {
-      const { id, value } = e.target;
       return {
         ...prevInfo,
         [id]: value
       }
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setDisplayInfo(prevInfo => {
+      return !prevInfo;
     });
   }
 
   return (
     <div className="general-container">
       <h2 className='title-text'>General Information</h2>
-      <form className="general-form">
-        <label htmlFor="name">Full Name: </label>
-        <input
-          className='general-input'
-          type="text"
-          placeholder="Full Name"
-          id="name"
-          value={info.name}
-          onChange={handleChange}
-        />
-        <label htmlFor="email">Email: </label>
-        <input
-          className='general-input'
-          type="email"
-          placeholder="Email"
-          id="email"
-          value={info.email}
-          onChange={handleChange}
-        />
-        <label htmlFor="phoneNumber">Phone Number: </label>
-        <input
-          className='general-input'
-          type="text"
-          placeholder="Phone Number"
-          id="phoneNumber"
-          value={info.phoneNumber}
-          onChange={handleChange}
-        />
-      </form>
-      <article className='general-text-container'>
-        <h3 className='general-text'>{info.name}</h3>
-        <h3 className='general-text'>{info.email}</h3>
-        <h3 className='general-text'>{info.phoneNumber}</h3>
-      </article>
+      {displayInfo ?
+        <article className='general-text-container'>
+          {/* {/* maybe loop over state and create h3s? */}
+          <h3 className='general-text'>{info.name}</h3>
+          <h3 className='general-text'>{info.email}</h3>
+          <h3 className='general-text'>{info.phoneNumber}</h3>
+        </article> :
+        <form onSubmit={handleSubmit} className="general-form">
+          <label htmlFor="name">Full Name: </label>
+          <input
+            className='general-input'
+            type="text"
+            placeholder="Full Name"
+            id="name"
+            value={info.name}
+            onChange={handleChange}
+          />
+          <label htmlFor="email">Email: </label>
+          <input
+            className='general-input'
+            type="email"
+            placeholder="Email"
+            id="email"
+            value={info.email}
+            onChange={handleChange}
+          />
+          <label htmlFor="phoneNumber">Phone Number: </label>
+          <input
+            className='general-input'
+            type="text"
+            placeholder="Phone Number"
+            id="phoneNumber"
+            value={info.phoneNumber}
+            onChange={handleChange}
+          />
+          <input type="submit" value="Submit" />
+        </form>}
     </div>
   );
 }

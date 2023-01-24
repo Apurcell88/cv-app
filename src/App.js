6c11e57caa366fb52ebabbe8e8325765ddb394b9
Adam Purcell <apurcell88@gmail.com>
2023-01-24 15:10:05 -0600

    Moved state and function to App and passed down to General via props.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,53 @@ import './App.css';
 import React from 'react';
 import Header from './components/Header';
 import GeneralInfo from './components/General';
+import Education from './components/Education';
 
 function App() {
+  const [info, setInfo] = React.useState({
+    name: '',
+    email: '',
+    phoneNumber: ''
+  });
+
+  const [displayInfo, setDisplayInfo] = React.useState(false);
+
+  const handleChange = (e, setState) => {
+    const { id, value } = e.target;
+    setState(prevInfo => {
+      return {
+        ...prevInfo,
+        [id]: value
+      }
+    })
+  }
+
+  const handleSubmit = (setState) => {
+    // e.preventDefault();
+    setState(prevInfo => {
+      return !prevInfo;
+    });
+  }
+
+  const handleEdit = (setState) => {
+    setState(false);
+  }
 
   return (
     <div className="App">
-      <Header />
-      <GeneralInfo />
+      <Header/>
+      <GeneralInfo
+        name={info.name}
+        email={info.email}
+        phoneNumber={info.phoneNumber}
+        setInfo={setInfo}
+        displayInfo={displayInfo}
+        setDisplayInfo={setDisplayInfo}
+        handleChange={handleChange}
+        handleEdit={handleEdit}
+        handleSubmit={handleSubmit}
+      />
+      <Education />
     </div>
   );
 }
--- src/components/General.js
+++ src/components/General.js
@@ -1,57 +1,59 @@
 import React from 'react';
 
 const GeneralInfo = (props) => {
-  // move state and handleChange to App.js and pass in as props?
-  const [info, setInfo] = React.useState({
-    name: '',
-    email: '',
-    phoneNumber: ''
-  });
+  // move state and functions into App.js and pass as props. All components need access to these functions
+  // const [info, setInfo] = React.useState({
+  //   name: '',
+  //   email: '',
+  //   phoneNumber: ''
+  // });
 
-  const [displayInfo, setDisplayInfo] = React.useState(false);
+  // const [displayInfo, setDisplayInfo] = React.useState(false);
 
-  const handleChange = (e) => {
-    // update state based on values from inputs
-    const { id, value } = e.target;
-    setInfo(prevInfo => {
-      return {
-        ...prevInfo,
-        [id]: value
-      }
-    })
-  }
+  // const handleChange = (e) => {
+  //   const { id, value } = e.target;
+  //   setInfo(prevInfo => {
+  //     return {
+  //       ...prevInfo,
+  //       [id]: value
+  //     }
+  //   })
+  // }
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setDisplayInfo(prevInfo => {
-      return !prevInfo;
-    });
-  }
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   setDisplayInfo(prevInfo => {
+  //     return !prevInfo;
+  //   });
+  // }
 
-  const handleEdit = () => {
-    setDisplayInfo(false);
-  }
+  // const handleEdit = () => {
+  //   setDisplayInfo(false);
+  // }
 
   return (
     <div className="general-container">
       <h2 className='title-text'>General Information</h2>
-      {displayInfo ?
+      {props.displayInfo ?
         <article className='general-text-container'>
           {/* {/* maybe loop over state and create h3s? */}
-          <h3 className='general-text'>{info.name}</h3>
-          <h3 className='general-text'>{info.email}</h3>
-          <h3 className='general-text'>{info.phoneNumber}</h3>
-          <button className='edit-btn' onClick={handleEdit}>Edit</button>
+          <h3 className='general-text'>{props.name}</h3>
+          <h3 className='general-text'>{props.email}</h3>
+          <h3 className='general-text'>{props.phoneNumber}</h3>
+          <button className='edit-btn' onClick={() => props.handleEdit(props.setDisplayInfo)}>Edit</button>
         </article> :
-        <form onSubmit={handleSubmit} className="general-form">
+        <form onSubmit={(e) => {
+          e.preventDefault()
+          props.handleSubmit(props.setDisplayInfo)
+        }} className="general-form">
           <label htmlFor="name">Full Name: </label>
           <input
             className='general-input'
             type="text"
             placeholder="Full Name"
             id="name"
-            value={info.name}
-            onChange={handleChange}
+            value={props.name}
+            onChange={(e) => props.handleChange(e, props.setInfo)}
           />
           <label htmlFor="email">Email: </label>
           <input
@@ -59,8 +61,8 @@ const GeneralInfo = (props) => {
             type="email"
             placeholder="Email"
             id="email"
-            value={info.email}
-            onChange={handleChange}
+            value={props.email}
+            onChange={(e) => props.handleChange(e, props.setInfo)}
           />
           <label htmlFor="phoneNumber">Phone Number: </label>
           <input
@@ -68,8 +70,8 @@ const GeneralInfo = (props) => {
             type="text"
             placeholder="Phone Number"
             id="phoneNumber"
-            value={info.phoneNumber}
-            onChange={handleChange}
+            value={props.phoneNumber}
+            onChange={(e) => props.handleChange(e, props.setInfo)}
           />
           <input
             type="submit"
